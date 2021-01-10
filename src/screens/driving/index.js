212b64a0c52bf90f8c015d900ef3604185b4b848
Hamza Ahmed <hamza.ahmed0402@gmail.com>
2021-01-11 04:45:55 +0800

    added image

--- src/screens/driving/index.js
+++ src/screens/driving/index.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 const Driving = props => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hey</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={require("../../../assets/car.png")}
+      />
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "column"
+  },
+  tinyLogo: {
+    width: 286,
+    height: 211
+  }
+});
 
 export default Driving;
