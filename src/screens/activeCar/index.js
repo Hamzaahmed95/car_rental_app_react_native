17028278ecd65022d32849a716fa406431b8e1c6
Hamza Ahmed <hamza.ahmed0402@gmail.com>
2021-01-11 16:26:07 +0800

    created reserved item car screen

---
+++ src/screens/activeCar/index.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { Text, TouchableOpacity, View, Image, StyleSheet } from "react-native";
+const ActiveCar = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={{ paddingLeft: 10, fontSize: 20, fontWeight: "500" }}>
+        Toyota Supra
+      </Text>
+      <Text style={{ paddingLeft: 10, fontSize: 14, fontWeight: "normal" }}>
+        VCG 7335
+      </Text>
+      <View style={styles.subContainer}>
+        <Image
+          style={styles.tinyLogo}
+          source={require("../../../assets/reserved.png")}
+        />
+      </View>
+      <View style={styles.subContent}>
+        <View>
+          <Text style={{ color: "rgba(0, 0, 0, 0.6)", fontSize: 12 }}>
+            Car Type
+          </Text>
+          <Text>Sedan</Text>
+        </View>
+        <View>
+          <Text style={{ color: "rgba(0, 0, 0, 0.6)", fontSize: 12 }}>
+            Seater
+          </Text>
+          <Text>6</Text>
+        </View>
+        <View>
+          <Text style={{ color: "rgba(0, 0, 0, 0.6)", fontSize: 12 }}>
+            Transmission
+          </Text>
+          <Text>Automatic</Text>
+        </View>
+      </View>
+      <View style={styles.statusContent}>
+        <Text style={{ fontSize: 16, color: "rgba(0, 0, 0, 0.6)" }}>
+          Booking starts at
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            color: "rgba(0, 0, 0, 0.6)",
+            fontWeight: "500"
+          }}
+        >
+          Wed 09/09/2020
+        </Text>
+      </View>
+      <View
+        style={{
+          marginTop: 20,
+          marginBottom: 20,
+          borderWidth: 0.5,
+          borderColor: "#ddd"
+        }}
+      />
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => {
+          // props.navigation.navigate("Reservation");
+        }}
+      >
+        <Text style={styles.buttonContent}>START DRIVING</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    marginTop: 15,
+    marginBottom: 0,
+    marginLeft: 15,
+    marginRight: 15,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "#ddd"
+  },
+  subContainer: {
+    alignItems: "center"
+  },
+  statusContent: {
+    alignItems: "center",
+    paddingTop: 30,
+    paddingBottom: 30
+  },
+  tinyLogo: {
+    width: 254,
+    height: 163
+  },
+  subContent: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  buttonContainer: {
+    margin: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonContent: {
+    fontFamily: "Roboto",
+    fontSize: 14,
+    alignItems: "center",
+    color: "#2F80ED"
+  }
+});
+
+export default ActiveCar;
--- src/screens/driving/index.js
+++ src/screens/driving/index.js
@@ -1,40 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { commonNavigation } from "../../utilities/AppUtils";
+import ActiveCar from "../activeCar/index";
 
 const Driving = props => {
+  const [driving, isDriving] = useState(true);
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.tinyLogo}
-        source={require("../../../assets/car.png")}
-      />
-      <Text style={styles.content}>Not driving a car yet</Text>
-      <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={() => {
-          props.navigation.navigate("Reservation");
-        }}
-      >
-        <Text style={styles.buttonContent}>MAKE A RESERVATION</Text>
-      </TouchableOpacity>
-      <View style={styles.bottomContainer}>
-        <Text style={styles.content}>Want to earn using your car?</Text>
-        <TouchableOpacity
-          style={styles.buttonBottomContainer}
-          onPress={() => {
-            props.navigation.navigate("CarDetail");
-          }}
-        >
-          <Text style={styles.buttonContent}>LIST YOUR CAR</Text>
-        </TouchableOpacity>
-      </View>
+      {driving ? (
+        <ActiveCar />
+      ) : (
+        <View style={styles.containersub}>
+          <Image
+            style={styles.tinyLogo}
+            source={require("../../../assets/car.png")}
+          />
+          <Text style={styles.content}>Not driving a car yet</Text>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => {
+              props.navigation.navigate("Reservation");
+            }}
+          >
+            <Text style={styles.buttonContent}>MAKE A RESERVATION</Text>
+          </TouchableOpacity>
+          <View style={styles.bottomContainer}>
+            <Text style={styles.content}>Want to earn using your car?</Text>
+            <TouchableOpacity
+              style={styles.buttonBottomContainer}
+              onPress={() => {
+                props.navigation.navigate("CarDetail");
+              }}
+            >
+              <Text style={styles.buttonContent}>LIST YOUR CAR</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff"
+  },
+  containersub: {
+    flex: 1,
+    justifyContent: "flex-start",
     alignItems: "center",
     flexDirection: "column"
   },
